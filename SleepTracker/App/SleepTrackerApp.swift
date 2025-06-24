4772990ea95ffa2838c4b6da70e6b133f92274f6
Vlasova Mariia <vlasovam014@gmail.com>
2025-06-25 01:26:04 +0300

    create project

---
+++ SleepTracker/App/SleepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTrackerApp.swift
+//  SleepTracker
+//
+//  Created by Мария Власова on 16.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SleepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
